aa0e7527af6d9262b9d3e715e7650785d926ba6f
mali-lang <m.ali@arytech.com>
2025-10-29 17:43:44 +0500

    Staging (#18)
    * Testing
    
    * Testing
    
    * Testing
    
    * Testing123 (#13)
    
    * Testing
    
    * Testing
    
    * Haseeb testing (#15)
    
    * Haseeb decline testing (#17)
    
    * Haseeb testing
    
    * Testing for decline ...

--- src/configs/dependencyInjection.conf.js
+++ src/configs/dependencyInjection.conf.js
@@ -2,5 +2,6 @@ module.exports = {
 	basePage: './components/frontend/basepage.js',
 	signUpPage: './components/frontend/OTTPlatform/Signup.js',
 	loginPage: './components/frontend/OTTPlatform/Login.js',
-
 }
+
+// Lets see the other change
