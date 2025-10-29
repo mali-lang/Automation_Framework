391105f0edb3f5fa88133810604ac900e112b3ae
Muhammad Ali <m.ali@arytech.com>
2025-10-29 13:44:12 +0500

    Testing

--- src/configs/dependencyInjection.conf.js
+++ src/configs/dependencyInjection.conf.js
@@ -2,5 +2,4 @@ module.exports = {
 	basePage: './components/frontend/basepage.js',
 	signUpPage: './components/frontend/OTTPlatform/Signup.js',
 	loginPage: './components/frontend/OTTPlatform/Login.js',
-
 }
